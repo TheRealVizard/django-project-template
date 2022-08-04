9a294c9724cc92aa9dc8a892b421c20fb7e2f29f
TheRealVizard <vizard@divineslns.com>
2022-08-03 22:08:29 -0400

    fixed directory tree

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 load_dotenv(dotenv_path=BASE_DIR / ".env")
 
 SECRET_KEY = os.environ.get("SECRET_KEY", None)
